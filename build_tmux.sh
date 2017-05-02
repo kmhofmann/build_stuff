ce02eea3e93189d199b1e4b0a36ce24d49050c75
Michael Hofmann <kmhofmann@gmail.com>
2017-05-03 00:10:10 +0200

    Some script changes and small fixes.

--- build_tmux.sh
+++ build_tmux.sh
@@ -1,27 +1,32 @@
 #!/usr/bin/env bash
 
-# TODO: user-configurable source and install directories
-
 if [ "$(uname -s)" == "Darwin" ]; then
   export NCPUS=`sysctl -n hw.ncpu`
 elif [ "$(uname -s)" == "Linux" ]; then
   export NCPUS=`nproc`
 fi
 
-CURRENT_DIR=$(pwd)
+# TODO: user-configurable source and install directories
 CLONE_DIR=$HOME/devel
-REPO_DIR=${CLONE_DIR}/tmux
+INSTALL_DIR=$HOME/local2
 
-INSTALL_DIR=$HOME/local
 echo "Cloning to ${REPO_DIR}, and installing to ${INSTALL_DIR}..."
-mkdir -p ${INSTALL_DIR}
 
+# Clone and get to clean slate
+mkdir -p ${CLONE_DIR}
 git -C ${CLONE_DIR} clone https://github.com/tmux/tmux.git
+REPO_DIR=${CLONE_DIR}/tmux
 git -C ${REPO_DIR} clean -fxd
 git -C ${REPO_DIR} pull origin master --rebase
 TMUX_VERSION=$(git -C ${REPO_DIR} describe --abbrev=0 --tags)
 git -C ${REPO_DIR} checkout ${TMUX_VERSION}
+
+# Compile and install
+CURRENT_DIR=$(pwd)
+mkdir -p ${INSTALL_DIR}
+
 cd ${REPO_DIR}
 sh autogen.sh
-./configure --prefix=${INSTALL_DIR} && make -j${NCPUS} && make installs
+./configure --prefix=${INSTALL_DIR} && make -j${NCPUS} && make install
+
 cd ${CURRENT_DIR}
--- build_vim.sh
+++ build_vim.sh
@@ -1,26 +1,40 @@
 #!/usr/bin/env bash
 
-# TODO: user-configurable source and install directories
-
 if [ "$(uname -s)" == "Darwin" ]; then
   export NCPUS=`sysctl -n hw.ncpu`
+  echo "Not tested on MacOS. But why not use Homebrew? Exiting..."
+  exit 1
 elif [ "$(uname -s)" == "Linux" ]; then
   export NCPUS=`nproc`
 fi
 
-CURRENT_DIR=$(pwd)
+# TODO: user-configurable source and install directories
 CLONE_DIR=$HOME/devel
-REPO_DIR=${CLONE_DIR}/vim
-
 INSTALL_DIR=$HOME/local
-echo "Cloning to ${REPO_DIR}, and installing to ${INSTALL_DIR}..."
-mkdir -p ${INSTALL_DIR}
 
+echo "Cloning to ${REPO_DIR}, and installing to ${INSTALL_DIR}..."
+
+# Prerequisites, according to
+# https://github.com/Valloric/YouCompleteMe/wiki/Building-Vim-from-source
+#
+# $ sudo apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev \
+#    libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
+#    libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
+#    python3-dev ruby-dev lua5.1 lua5.1-dev libperl-dev git
+
+# Clone and get to clean slate
+mkdir -p ${CLONE_DIR}
 git -C ${CLONE_DIR} clone https://github.com/vim/vim.git
+REPO_DIR=${CLONE_DIR}/vim
 git -C ${REPO_DIR} clean -fxd
 git -C ${REPO_DIR} pull origin master --rebase
 git -C ${REPO_DIR} checkout master
+
+# Compile and install
+CURRENT_DIR=$(pwd)
+mkdir -p ${INSTALL_DIR}
 cd ${REPO_DIR}
+
 ./configure \
     --prefix=${INSTALL_DIR} \
     --with-features=huge \
@@ -34,4 +48,5 @@ cd ${REPO_DIR}
     --enable-cscope \
     --prefix=${INSTALL_DIR}
 make -j${NCPUS} && make install
+
 cd ${CURRENT_DIR}
