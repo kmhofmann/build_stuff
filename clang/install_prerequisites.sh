e5a27cdbcb7edac9b921eed0ab0ab30d51fb6013
Michael Hofmann <kmhofmann@gmail.com>
2018-02-22 19:50:39 +0100

    Clang: added Debian dependencies.

--- clang/install_prerequisites.sh
+++ clang/install_prerequisites.sh
@@ -6,5 +6,6 @@ if [ "$(uname -s)" == "Darwin" ]; then
   brew install swig cmake
   # TODO: maybe more on a fresh installation?
 elif [ "$(uname -s)" == "Linux" ]; then
-  sudo apt-get install build-essential autoconf doxygen swig libedit-dev cmake
+  sudo apt-get install build-essential autoconf doxygen swig libedit-dev cmake \
+    subversion python3 libpython3-dev libxml2-dev libz3-dev libncurses-dev
 fi
